5a71b0a58d9157f787d94e4de8def51eec99c532
berkleybledsoe-afk <berkleybledsoe@gmail.com>
2026-01-02 17:29:21 -0600

    Create code.py
    Coded in python

---
+++ code.py
@@ -0,0 +1,11 @@
+item_1 = input("Enter Item 1 Name :")
+price_1 = float(input("Enter the price of Item 1:"))
+quantity_1 = int(input("Enter the quantity of Item 1: "))
+
+item_2 = input("Enter Item 2 Name:")
+price_2 = float(input("Enter the price of Item 2:"))
+quantity_2 = int(input("Enter the quantity of Item 2: "))
+
+total = quantity_1 * price_1 + quantity_2 * price_2
+print(f"You have bought {quantity_1} {item_1} for ${price_1} and {quantity_2} {item_2} for ${price_2}")
+print(f"Your total price is ${total}")
