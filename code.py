d8ea60a652ee478a4cfa324f2cb1320e3bafacd9
berkleybledsoe-afk <berkleybledsoe@gmail.com>
2026-01-02 17:40:30 -0600

    Update code.py

--- code.py
+++ code.py
@@ -7,5 +7,4 @@ price_2 = float(input("Enter the price of Item 2:"))
 quantity_2 = int(input("Enter the quantity of Item 2: "))
 
 total = quantity_1 * price_1 + quantity_2 * price_2
-print(f"You have bought {quantity_1} {item_1} for ${price_1} and {quantity_2} {item_2} for ${price_2}")
-print(f"Your total price is ${total}")
+print(f"You have bought {quantity_1} {item_1} and {quantity_2} {item_2} for a total price of ${total}")
